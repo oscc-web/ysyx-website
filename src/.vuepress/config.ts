b7cbb06c1cbcf60c40e7b1fea1ce49fdb3c8015f
myyerrol <myyerrol@126.com>
2023-06-17 11:36:22 +0800

    feat: configure search plugin

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,6 +7,7 @@ import Icons from "unplugin-icons/vite"
 import IconsResolver from "unplugin-icons/resolver"
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 import theme from "./theme.js";
 
@@ -43,6 +44,28 @@ export default defineUserConfig({
         }),
         registerComponentsPlugin({
             componentsDir: path.resolve(__dirname, "./components")
+        }),
+        searchProPlugin({
+            indexContent: true,
+            autoSuggestions: true,
+            customFields: [{
+                getter: (page) => page.frontmatter.title,
+                formatter: "标题：$content"
+            }, {
+                getter: (page) => page.frontmatter.author,
+                formatter: "作者：$content"
+            }, {
+                getter: (page) => page.frontmatter.category,
+                formatter: "分类：$content"
+            }, {
+                getter: (page) => page.frontmatter.tag,
+                formatter: "标签：$content"
+            }],
+            hotKeys: [{ key: "k", ctrl: true }, { key: "/", ctrl: true }],
+            queryHistoryCount: 5,
+            resultHistoryCount: 5,
+            delay: 150,
+            sortStrategy: "max"
         })
     ],
     theme: theme
