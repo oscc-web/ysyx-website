f7a3bd4ca60b9604cdef8eebefbd2192c872ea3e
myyerrol <myyerrol@126.com>
2023-08-16 14:42:43 +0800

    feat: update job contents

--- setup.sh
+++ setup.sh
@@ -8,15 +8,15 @@ PATH_NODE=${PATH_ROOT}/node_modules/
 
 # Set resources
 if [ ! -d ${PATH_RESS}/.git ]; then
-    rm -rf ${PATH_RESS}
-    git clone git@github.com:oscc-ysyx-web-project/ysyx-website-resources.git ${PATH_RESS}
+	rm -rf ${PATH_RESS}
+	git clone git@github.com:oscc-web/ysyx-website-resources.git ${PATH_RESS}
 else
-    cd ${PATH_RESS}
-    git pull origin main
-    cd ${PATH_ROOT}
+	cd ${PATH_RESS}
+	git pull origin main
+	cd ${PATH_ROOT}
 fi
 
 if [ ! -d ${PATH_NODE} ]; then
-    # Install packages
-    npm install
+	# Install packages
+	npm install
 fi
