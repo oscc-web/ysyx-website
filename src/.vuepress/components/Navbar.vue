fa109c84fd75237488102eddd122d36cfd56b584
myyerrol <myyerrol@126.com>
2023-12-10 08:13:05 +0800

    chore: delete unused components

--- src/.vuepress/components/Navbar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <Navbar>
-    </Navbar>
-</template>
-<script setup lang="ts">
-    import Navbar from "vuepress-theme-hope/modules/navbar/components/Navbar.js";
-</script>
--- src/.vuepress/components/NavbarLinks.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <NavbarLinks>
-    </NavbarLinks>
-</template>
-<script setup lang="ts">
-    import NavbarLinks from "vuepress-theme-hope/modules/navbar/components/NavbarLinks.js";
-</script>
