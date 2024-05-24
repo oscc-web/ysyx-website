8046c5b78c0bb0ba6280a0f34e719dddcc494cc5
myyerrol <myyerrol@126.com>
2024-05-24 16:16:40 +0800

    docs: hide unused files

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -19,35 +19,41 @@ export const enNavbar = navbar([
     }, {
         text: "Course",
         link: "https://ysyx.oscc.cc/docs/en/"
-    }, {
-        text: "Study Record",
-        link: "https://docs.qq.com/sheet/DT29weHpIRWNnYlFl?tab=BB08J2"
-    }, {
-        text: "Tape-Out & Board",
-        prefix: "/en/chip/",
-        children: [
-            "tapeout/",
-            "board/official/"
-        ]
-    }, {
-        text: "Certificate",
-        link: "/en/cert/"
-    }, {
-        text: "SIG",
-        link: "/en/sig/"
-    }, {
-        text: "Recruitment",
-        prefix: "/en/job/",
-        children: ["team", {
-            text: "External",
-            prefix: "external/",
-            children: [
-                "apply",
-                "contents"
-            ]
-        }]
-    }, {
-        text: "Contact US",
+    },
+    // {
+    //     text: "Study Record",
+    //     link: "https://docs.qq.com/sheet/DT29weHpIRWNnYlFl?tab=BB08J2"
+    // },
+    // {
+    //     text: "Tape-Out & Board",
+    //     prefix: "/en/chip/",
+    //     children: [
+    //         "tapeout/",
+    //         "board/official/"
+    //     ]
+    // },
+    // {
+    //     text: "Certificate",
+    //     link: "/en/cert/"
+    // },
+    // {
+    //     text: "SIG",
+    //     link: "/en/sig/"
+    // },
+    // {
+    //     text: "Recruitment",
+    //     prefix: "/en/job/",
+    //     children: ["team", {
+    //         text: "External",
+    //         prefix: "external/",
+    //         children: [
+    //             "apply",
+    //             "contents"
+    //         ]
+    //     }]
+    // },
+    {
+        text: "Contact us",
         link: "/en/contact/"
     }
 ]);
